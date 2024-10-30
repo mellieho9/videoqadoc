bb81ebc848103a711a89860b8c72f40f196f8718
mellieho9 <melioraho9@gmail.com>
2024-10-30 16:16:48 -0400

    added a video player component

--- videoqadoc/app/task/page.tsx
+++ videoqadoc/app/task/page.tsx
@@ -4,6 +4,7 @@ import TaskBar from "@/components/controller/taskBar";
 import { FieldSet } from "@/components/input/fieldSet";
 import TimeRangeContainer from "@/components/input/timeRangeContainer";
 import { Button, Divider } from "@nextui-org/react";
+import { YouTubeEmbed } from "@next/third-parties/google";
 
 export default function Page() {
   const options = ["option 1", "option 2", "option 3", "option 4"];
@@ -24,10 +25,8 @@ export default function Page() {
       <TaskBar />
       <section className="w-full h-full flex flex-col md:flex-row items-center justify-center p-10 gap-4">
         {/* video feed */}
-        <div className="w-full md:w-3/4 h-full bg-slate-700 p-5 rounded-md">
-          <div className="w-full h-full rounded-md flex items-center justify-center">
-            <span className="text-white">Video Player Placeholder</span>
-          </div>
+        <div className="w-full md:w-3/4rounded-md">
+          <YouTubeEmbed videoid="yptflB597IQ" />
         </div>
 
         <div className="flex flex-col p-5 gap-4">
